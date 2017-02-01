74f844bbcd27bf7d3c7ac391486abe33a2a2f93f
yuki <starlod.ggl@gmail.com>
2017-02-01 21:21:44 +0900

    バージョニングうまくいかなかったのでバージョン設定を元に戻す。

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,8 +12,4 @@ const { mix } = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .version([
-        'public/css/app.css',
-        'public/js/app.js'
-   ]);
+   .sass('resources/assets/sass/app.scss', 'public/css');
