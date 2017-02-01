cd650e482ff9c7dfb6798b0ed3e774ce6be78c23
yuki <starlod.ggl@gmail.com>
2017-02-01 21:12:07 +0900

    バージョニング

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,6 @@ const { mix } = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css')
    .version([
-        'css/app.css',
-        'js/app.js'
+        'public/css/app.css',
+        'public/js/app.js'
    ]);
