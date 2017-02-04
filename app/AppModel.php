42f68888382191cf224e21d3274a42a01c394bd0
yuki <starlod.ggl@gmail.com>
2017-02-04 17:26:45 +0900

    親モデル

---
+++ app/AppModel.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+use Auth;
+
+class AppModel extends Model
+{
+    /**
+     * timestampsを利用する
+     *
+     */
+    public $timestamps = true;
+
+    protected $orderBy;
+    protected $orderDirection = 'desc';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function($model) {
+            $model->created_by_id = Auth::user()->id;
+            $model->updated_by_id = $model->created_by_id;
+        });
+
+        static::updating(function($model) {
+            $model->updated_by_id = Auth::user()->id;
+        });
+    }
+
+    public function scopeOrdered($query)
+    {
+        if ($this->orderBy) {
+            return $query->orderBy($this->orderBy, $this->orderDirection);
+        }
+        return $query;
+    }
+
+    public function scopeGetOrdered($query)
+    {
+        return $this->scopeOrdered($query)->get();
+    }
+
+    /**
+     * 一番古い登録日付を取得（存在しない場合は今日の日付を返す）
+     */
+    public function scopeFirstCreatedAt($query)
+    {
+        if ($first = $query->orderBy('created_at', 'asc')->get()->first()) {
+            return $first->created_at;
+        }
+
+        return new Carbon();
+    }
+
+    /**
+     * 検索
+     * 各モデル内に検索するカラムのスコープを書く。
+     */
+    public function scopeSearch($query, $keywords)
+    {
+        foreach ($keywords as $name => $keyword) {
+            if ($keyword !== '') {
+                $method_name = 'search' . pascal_case($name);
+                if (method_exists($this, 'scope' . $method_name)) {
+                    $query->$method_name($keyword);
+                }
+            }
+        }
+    }
+}
