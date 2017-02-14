87338a2384a2ea2022cb087599a87c867ab0af1b
yuki <starlod.ggl@gmail.com>
2017-02-14 21:22:49 +0900

    Laravel/Duskの導入
    composer require laravel/dusk
    php artisan dusk:install

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Dusk\DuskServiceProvider;
 
 use Faker\Generator as FakerGenerator;
 use Faker\Factory as FakerFactory;
@@ -29,5 +30,9 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(FakerGenerator::class, function () {
             return FakerFactory::create('ja_JP');
         });
+
+        if ($this->app->environment('local', 'testing')) {
+            $this->app->register(DuskServiceProvider::class);
+        }
     }
 }
