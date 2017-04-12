e8f6435d28fb0d3813e0c7bb526907f1d9f0fce9
yuki <starlod.ggl@gmail.com>
2017-04-12 21:19:03 +0900

    provision時のみsourceMapを出力

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,11 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js('resources/assets/js/app.js', 'public/js');
+mix.sass('resources/assets/sass/app.scss', 'public/css');
 
 // Versioning
 if (mix.config.inProduction) {
+    mix.sourceMaps();
     mix.version();
 }
