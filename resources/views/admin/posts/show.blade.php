dff8d19446b68fff413e5a553d69b8d616e504cb
yuki <starlod.ggl@gmail.com>
2017-06-13 21:29:34 +0900

    マークダウンtoHTML変換

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -5,7 +5,7 @@
 <div id="post" class="container">
     <h1 class="title">{{ $post->title }}</h1>
 
-    <div>{{ $post->content }}</div>
+    <div>{!! Markdown::convertToHtml($post->content) !!}</div>
 
     {{ link_to_route('admin.posts.edit', trans('messages.buttons.edit'), $post->id, ['class' => 'btn btn-secondary', 'role' => 'button']) }}
 </div>
