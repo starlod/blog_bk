234419e8b1f2758f41f920fc3f43987ec300178b
yuki <starlod.ggl@gmail.com>
2017-06-13 21:29:18 +0900

    アラートレイアウトの調整

--- resources/views/_messages.blade.php
+++ resources/views/_messages.blade.php
@@ -1,8 +1,7 @@
 @if (Session::has('messages'))
     @foreach (Session::pull('messages') as $type => $messages)
-        <div class="alert alert-dismissible alert-{{ $type }} m-b-md no-print" role="alert">
-            <button type="button" class="close" data-dismiss="alert">×</button>
-            <ul>
+        <div class="alert alert-{{ $type }}" role="alert">
+            <ul class="m-a-0 p-a-0">
                 @foreach($messages as $message)
                     <li>{!! $message !!}</li>
                 @endforeach
@@ -12,7 +11,7 @@
 @endif
 
 @if (count($errors))
-    <div class="alert alert-dismissible alert-danger m-b-md no-print" role="alert">
+    <div class="alert alert-dismissible alert-danger no-print" role="alert">
         <button type="button" class="close" data-dismiss="alert">×</button>
         <ul>
             @foreach($errors->all() as $error)
